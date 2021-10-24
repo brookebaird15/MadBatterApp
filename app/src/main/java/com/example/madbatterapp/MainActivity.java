5b0a63736d5682ab351a4f9c5801f95c4d2be9d1
Brooke Baird <w0766373@myscc.ca>
2021-10-24 01:32:51 -0400

    Issue #1: Fix Issues
    - Navigation to home wasn't set up correctly
    - Fixed in Main Activiy

--- app/src/main/java/com/example/madbatterapp/MainActivity.java
+++ app/src/main/java/com/example/madbatterapp/MainActivity.java
@@ -25,17 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        setSupportActionBar(binding.appBarMain.toolbar);
-//        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
