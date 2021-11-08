b19651fa2cce2bb2f048071e1d0d073d0b1a09cd
Brooke Baird <w0766373@myscc.ca>
2021-11-07 21:18:00 -0500

    #1 Add CustomCategoryHolder class to the Adapter

--- app/src/main/java/com/example/madbatterapp/HomeViewAdapter/CustomCategoryHolder.java
+++ app/src/main/java/com/example/madbatterapp/HomeViewAdapter/CustomCategoryHolder.java
@@ -1,4 +1,38 @@
 package com.example.madbatterapp.HomeViewAdapter;
 
-public class CustomCategoryHolder {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.madbatterapp.R;
+
+public class CustomCategoryViewAdapter extends RecyclerView.Adapter<CustomCategoryHolder>{
+
+    @NonNull
+    @Override
+    public CustomCategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomCategoryHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+public class CustomCategoryHolder extends RecyclerView.ViewHolder {
+    protected ImageView image;
+
+    public CustomCategoryHolder(@NonNull View itemView){
+        super(itemView);
+        this.image = itemView.findViewById(R.id.categoryBtn);
+
+    }
 }
