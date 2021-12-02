3762eb59cbcbd7142204352dd47cbb2a6e329d9c
Brooke Baird <w0766373@myscc.ca>
2021-12-01 23:25:25 -0500

    #6, #14 Add email intent to the checkout button

--- app/src/main/java/com/example/madbatterapp/Fragments/CartFragment.java
+++ app/src/main/java/com/example/madbatterapp/Fragments/CartFragment.java
@@ -1,5 +1,7 @@
 package com.example.madbatterapp.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,6 +20,7 @@ import com.example.madbatterapp.MainActivity;
 import com.example.madbatterapp.Product;
 import com.example.madbatterapp.R;
 import com.example.madbatterapp.ShoppingCartCatalogue;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,10 +33,34 @@ public class CartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_cart, container, false);
+
         MainActivity.fab.hide();
         RecyclerView recyclerView = view.findViewById(R.id.productCatalogRecyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
         recyclerView.setAdapter(new CustomCartViewAdapter(ShoppingCartCatalogue.getInstance().getCart(), getContext()));
+
+        Button checkoutBtn = view.findViewById(R.id.checkoutBtn);
+        if (ShoppingCartCatalogue.getInstance().getCart() != null) {
+            checkoutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String[] emailAddress = {"madbatterbakery@gmail.com"};
+                    Intent i = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
+                    i.putExtra(Intent.EXTRA_EMAIL, emailAddress);
+                    i.putExtra(Intent.EXTRA_TEXT, "Thank you for your purchase of: " + ShoppingCartCatalogue.getInstance().getCart() + ". " +
+                            "You will receive an email when your order is ready for pickup. Please pay in person for your order");
+                    startActivity(i);
+                }
+            });
+        } else {
+            checkoutBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Your cart is empty! Please add to the cart.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
         return view;
     }
 }
