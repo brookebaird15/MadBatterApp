4163dc7ab12f4f83e3a20523692044151ad3f0ce
Brooke Baird <w0766373@myscc.ca>
2021-12-06 01:45:32 -0500

    #6 Textviews lead to actual websites on click

--- app/src/main/java/com/example/madbatterapp/Fragments/CreditsFragment.java
+++ app/src/main/java/com/example/madbatterapp/Fragments/CreditsFragment.java
@@ -13,17 +13,65 @@ import android.widget.TextView;
 import com.example.madbatterapp.R;
 
 public class CreditsFragment extends Fragment {
+    TextView getapp;
 
-
-
-
+    /**
+     * All this is really doing is making so the textviews when click, link to html
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_credits, container, false);
-        TextView getapp = view.findViewById(R.id.cookieCredit1);
+        getapp = view.findViewById(R.id.cookieCredit1);
         getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cookieCredit2);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cookieCredit3);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cookieCredit8);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cookieCredit9);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.brownieCredit1);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.brownieCredit2);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.brownieCredit3);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.brownieCredit4);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit1);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit2);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit3);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit3);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit4);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit5);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit6);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit7);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit8);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.cakeCredit9);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.otherCredit1);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.otherCredit5);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+        getapp = view.findViewById(R.id.otherCredit6);
+        getapp.setMovementMethod(LinkMovementMethod.getInstance());
+
         return view;
     }
 }
