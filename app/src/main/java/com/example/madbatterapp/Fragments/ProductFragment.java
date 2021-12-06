4f6b8a54e22f0a9fb6f1784e04865412eeca6f5d
Brooke Baird <w0766373@myscc.ca>
2021-12-06 04:00:17 -0500

    #50 Allow user to adjust grid layout - Allow user to view more product on screen

--- app/src/main/java/com/example/madbatterapp/Fragments/ProductFragment.java
+++ app/src/main/java/com/example/madbatterapp/Fragments/ProductFragment.java
@@ -1,11 +1,13 @@
 package com.example.madbatterapp.Fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,12 +23,17 @@ public class ProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_product, container, false);
-
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        boolean layout = sharedPreferences.getBoolean("gridLayout", false);
         //Use either one adapter or the other depending on the screen width
         RecyclerView recyclerView = view.findViewById(R.id.largeProductRecyclerView);
         if (recyclerView != null) {
-            //Large Screen
-            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+            if (layout) {
+                //Large Screen
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 4));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+            }
         } else {
             //small screen
             recyclerView = view.findViewById(R.id.productRecyclerView);
